fa8124078338050d341cff1822ca793392c6cf14
yidye <yidnekachewtebeje@gmail.com>
2022-12-20 12:51:54 -0800

    format document

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,11 +3,11 @@
 0. Pascal's Triangle
 """
 
+
 def pascal_triangle(n):
     """Create a function def pascal_triangle(n): that returns a list of lists
     of integers representing the Pascal’s triangle of n
     """
-    
     if n <= 0:
         return []
     triangle = [[1]]
